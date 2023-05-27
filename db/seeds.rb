7fd137e9a0a9a51f336cb48519eafc41b96a74f4
sebastian medina <sebastianlopez_21@hotmail.com>
2023-05-26 23:02:49 -0400

    Adds Dealer migration and model

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Dealer.create(name: "Used Auto World" , logo: "https://e7.pngegg.com/pngimages/40/772/png-clipart-car-dealership-alsa-enterprises-motors-used-car-vehicle-car-blue-text.png", location: "45 Main Street, New York, NY 10001");
+Dealer.create(name: "CarDeals", logo: "https://www.clipartmax.com/png/middle/39-396570_8900-car-dealer-logo-png.png", location: "56 Oak Avenue, Chicago, IL 60601" );
+Dealer.create(name: "Champion Auto Sales", logo: "https://image.pngaaa.com/150/321150-middle.png" , location: "453 Elm Street, Los Angeles, CA 90001")
+Dealer.create(name: "Ace Auto Sales", logo: "https://w1.pngwing.com/pngs/367/247/png-transparent-vip-logo-car-car-dealership-used-car-engine-motor-vehicle-service-convertible-red.png", location: "12 Maple Drive, Houston, TX 77001" )
+Dealer.create(name: "Bakwena Motor Sales", logo: "https://www.vhv.rs/dpng/d/577-5777773_car-dealership-car-sale-logo-hd-png-download.png" , location: "4 Cedar lane, Phoenix, FL 85001" )
